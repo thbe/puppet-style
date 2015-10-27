8ae311e06dbe485f5fbe7878fe29869c391aaf00
thbe <project@bendler-net.de>
2015-10-27 18:42:33 +0100

    Remove versions from Gemfile

--- spec/classes/style_spec.rb
+++ spec/classes/style_spec.rb
@@ -10,50 +10,33 @@ describe "style" do
 
       it { is_expected.to compile.with_all_deps }
 
+      it { is_expected.to contain_class('style::package') }
+      it { is_expected.to contain_class('style::config') }
+      it { is_expected.to contain_package('dmidecode') }
+      it { is_expected.to contain_package('dstat') }
+      it { is_expected.to contain_package('htop') }
+      it { is_expected.to contain_package('iftop') }
+      it { is_expected.to contain_package('iotop') }
+      it { is_expected.to contain_package('ncdu') }
+      it { is_expected.to contain_package('ncftp') }
+      it { is_expected.to contain_package('nmap') }
+      it { is_expected.to contain_package('pv') }
+      it { is_expected.to contain_package('rsync') }
+      it { is_expected.to contain_package('screen') }
+      it { is_expected.to contain_package('strace') }
+      it { is_expected.to contain_package('tree') }
+      it { is_expected.to contain_package('unzip') }
+      it { is_expected.to contain_package('wget') }
+      it { is_expected.to contain_file('/etc/profile.d/custom.sh') }
+
       case facts[:osfamily]
       when 'Debian'
-        it { is_expected.to contain_class('style::package') }
-        it { is_expected.to contain_class('style::config') }
-        it { is_expected.to contain_package('dmidecode') }
-        it { is_expected.to contain_package('dstat') }
         it { is_expected.to contain_package('glances') }
-        it { is_expected.to contain_package('htop') }
-        it { is_expected.to contain_package('iftop') }
-        it { is_expected.to contain_package('iotop') }
-        it { is_expected.to contain_package('ncdu') }
-        it { is_expected.to contain_package('ncftp') }
-        it { is_expected.to contain_package('nmap') }
-        it { is_expected.to contain_package('pv') }
-        it { is_expected.to contain_package('rsync') }
-        it { is_expected.to contain_package('screen') }
-        it { is_expected.to contain_package('strace') }
-        it { is_expected.to contain_package('tree') }
-        it { is_expected.to contain_package('unzip') }
         it { is_expected.to contain_package('vim') }
-        it { is_expected.to contain_package('wget') }
         it { is_expected.to contain_package('xauth') }
-        it { is_expected.to contain_file('/etc/profile.d/custom.sh') }
       when 'RedHat'
-        it { is_expected.to contain_class('style::package') }
-        it { is_expected.to contain_class('style::config') }
-        it { is_expected.to contain_package('dmidecode') }
-        it { is_expected.to contain_package('dstat') }
-        it { is_expected.to contain_package('htop') }
-        it { is_expected.to contain_package('iftop') }
-        it { is_expected.to contain_package('iotop') }
-        it { is_expected.to contain_package('ncdu') }
-        it { is_expected.to contain_package('ncftp') }
-        it { is_expected.to contain_package('nmap') }
-        it { is_expected.to contain_package('pv') }
-        it { is_expected.to contain_package('rsync') }
-        it { is_expected.to contain_package('screen') }
-        it { is_expected.to contain_package('strace') }
-        it { is_expected.to contain_package('tree') }
-        it { is_expected.to contain_package('unzip') }
         it { is_expected.to contain_package('vim-enhanced') }
-        it { is_expected.to contain_package('wget') }
         it { is_expected.to contain_package('xorg-x11-xauth') }
-        it { is_expected.to contain_file('/etc/profile.d/custom.sh') }
         if facts[:lsbmajdistrelease] = '5'
           it { is_expected.to contain_package('redhat-lsb') }
         else
